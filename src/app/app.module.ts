4c86fceeae8665cb5994bf0f2dbefa9ac0064e1f
Lazarus LaRue <lazaruslarue@gmail.com>
2017-12-17 11:33:45 -0700

    add stepper module. add stepper component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CovalentLayoutModule, CovalentStepsModule } from '@covalent/core';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
 
 import { AppComponent } from './app.component';
-
-
+import { StepperEntryFormModule } from './stepper-entry-form/stepper-entry-form.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    CovalentDynamicFormsModule,
+    //
+    StepperEntryFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
